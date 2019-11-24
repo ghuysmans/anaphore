186a724c4b29dcdd69114ddc12752606f0ef2dae
Guillaume Huysmans <ghuysmans99@gmail.com>
2019-11-24 23:46:09 +0100

    Better Hanoi

--- hanoi.ml
+++ hanoi.ml
@@ -1,32 +1,19 @@
-type t =
-  | A
-  | B
-  | C
-
-let pp ppf t =
-  Printf.fprintf ppf "%s" (match t with
-    | A -> "A"
-    | B -> "B"
-    | C -> "C"
-  )
-
 let move from to_ =
-  (* Printf.printf "%a -> %a\n" pp from pp to_ *)
-  Printf.printf "%a%a\n" pp from pp to_
+  Printf.printf "%c%c\n" from to_
 
-let rec move_n ~via from to_ = function
+let rec move_n ~via:t x y = function
   | 0 -> ()
   | n -> (
-    move_n from via ~via:to_ (pred n);
-    move from to_;
-    move_n via to_ ~via:from (pred n)
+    move_n x t ~via:y (pred n);
+    move x y;
+    move_n t y ~via:x (pred n)
   )
 
 
 let () =
   match Sys.argv with
   | [| _; n |] ->
-    move_n A C ~via:B (int_of_string n)
+    move_n 'A' 'C' ~via:'B' (int_of_string n)
   | _ ->
     Printf.eprintf "usage: %s n\n" Sys.argv.(0);
     exit 1
